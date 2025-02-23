60c97a754b5e567af356219b848330829d893ff8
ERIK MOLINA <efmolina@iastate.edu>
2025-02-23 13:17:24 -0600

    Modified DELETEmapping in User Controller for deleting a user

--- tailgate_backend/src/main/java/com/example/tailgate/tailgate_backend/controller/UserController.java
+++ tailgate_backend/src/main/java/com/example/tailgate/tailgate_backend/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * This class is a Spring REST controller that handles user registration and fetching all users
@@ -34,6 +35,26 @@ public class UserController {
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+        System.out.println("Received User: " + user);
+
+        // Check if the user exists in the database
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.ok(Map.of(
+                    "success", true,
+                    "message", "User logged in successfully"));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of(
+                            "success", false,
+                            "error", "Invalid email or password"
+                    ));
+        }
+    }
+
 
     /** POSTS */
     @PostMapping("/register")
@@ -63,6 +84,38 @@ public class UserController {
         }
 
     }
+    /** DEL */
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteUser(@RequestHeader("username") String username,
+                                        @RequestHeader("email") String email,
+                                        @RequestHeader("password") String password){
+
+        System.out.println("Received User: " + username + " " + email);
+
+        // Find user in the database by email
+        Optional<User> existingUser = userRepository.findByEmail(email);
+
+        // Check if the user exists and the password matches
+        if (existingUser.isPresent()){
+
+            // Check if the username, password, and email match
+            if (existingUser.get().getUsername().equals(username) &&
+                existingUser.get().getPassword().equals(password) &&
+                existingUser.get().getEmail().equals(email)) {
+
+                userRepository.delete(existingUser.get());
+                return ResponseEntity.ok(Map.of("success", true, "message", "User deleted successfully"));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body(Map.of("success", false, "error", "Invalid User Credentials"));
+            }
+
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("success", false, "error", "User not found"));
+        }
+
+    }
 
 
     /** GETS */
@@ -71,5 +124,7 @@ public class UserController {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+
 
 }
